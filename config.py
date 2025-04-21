914900237bf7bc5473fc94d0d5c39c86f3d7d643
Federico De Paoli <120582465+F-Depi@users.noreply.github.com>
2025-04-21 20:38:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    'host': os.getenv('DB_HOST', '192.168.1.111'),
+    'host': os.getenv('DB_HOST', 'localhost'),
     'database': os.getenv('DB_NAME', 'athletics_stats'),
     'user': os.getenv('DB_USER', 'postgres'),
     'password': os.getenv('DB_PASSWORD', '1234'),
